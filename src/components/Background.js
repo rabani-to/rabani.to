7acbf7ed5d6c458827f0525bcb3851a4045f35b7
D3Portillo <d3portillo@gmail.com>
2022-10-07 03:24:56 +0200

    fix(Background.js): now video adapts to screen width

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,8 +1,13 @@
 function Background() {
   return (
-    <div className="cursor-none absolute inset-0">
+    <div className="cursor-none absolute inset-0 flex items-center">
       <div className="bg-black/75 fixed inset-0" />
-      <video loop className="w-full" src="/boo.mp4" autoPlay />
+      <video
+        loop
+        className="w-full h-full object-cover"
+        src="/boo.mp4"
+        autoPlay
+      />
     </div>
   )
 }
