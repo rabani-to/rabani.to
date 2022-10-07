563e785a17756e4b2a9b4edc12553f2375cd03cc
D3Portillo <d3portillo@gmail.com>
2022-10-07 03:13:14 +0200

    chores: enhance manifesto layout

--- src/pages/manifesto.js
+++ src/pages/manifesto.js
@@ -10,8 +10,49 @@ export default function ManifestoPage() {
       <SeoTags />
       <Layout>
         <Navigation />
-        <div className="min-h-screen">
-          <h2 className="text-7xl font-roboto font-bold">Manifesto</h2>
+        <div className="min-h-screen flex flex-col lg:flex-row gap-12">
+          <section className="lg:text-right overflow-hidden">
+            <h2 className="text-[6rem] leading-none font-roboto font-bold">
+              The
+              <br />
+              Radish <br />
+              Manifesto.
+            </h2>
+            <p className="mt-10 text-gray-400">
+              We are an innovation network working with Web3 primitive to
+              design, develope and impore blocjaeconae aeloremp ipsim loremp
+              impsum loremp impsumloremp.
+            </p>
+          </section>
+          <section className="max-w-3xl text-lg flex flex-col gap-4 text-gray-400">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+              sit impedit accusantium amet eos non nesciunt! Laboriosam
+              incidunt, rerum, exercitationem voluptas maiores repellat earum
+              quae necessitatibus tenetur, dolorem non beatae!
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+              sit impedit accusantium amet eos non nesciunt! Laboriosam
+              incidunt, rerum, exercitationem voluptas maiores repellat earum
+              quae necessitatibus tenetur, dolorem non beatae!
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+              sit impedit accusantium amet eos non nesciunt! Laboriosam
+              incidunt, rerum, exercitationem voluptas maiores repellat earum
+              quae necessitatibus tenetur, dolorem non beatae!
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+              sit impedit accusantium amet eos non nesciunt! Laboriosam
+              incidunt, rerum, exercitationem voluptas maiores repellat earum
+              quae necessitatibus tenetur, dolorem non beatae!
+            </p>
+          </section>
         </div>
       </Layout>
     </Fragment>
