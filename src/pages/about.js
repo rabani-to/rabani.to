197ab937d5e62b0609b8c1dfa9f935647363548e
D3Portillo <d3portillo@gmail.com>
2022-10-07 03:12:59 +0200

    feat(about.js): add page content

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,17 +3,113 @@ import { Fragment } from "react"
 import Navigation from "@/components/Navigation"
 import Layout from "@/components/Layout"
 import SeoTags from "@/components/SeoTags"
-
+import Link from "next/link"
+import asset_denny from "@/assets/denny.png"
+import asset_valentin from "@/assets/valentin.jpg"
+import asset_diego from "@/assets/diego.jpg"
+import { GrLinkedinOption } from "react-icons/gr"
+import { BsGithub, BsLinkedin, BsTwitter } from "react-icons/bs"
+import { ImLinkedin2 } from "react-icons/im"
+import { FaLinkedinIn } from "react-icons/fa"
+import Image from "next/image"
 export default function AboutPage() {
   return (
     <Fragment>
       <SeoTags />
       <Layout>
         <Navigation />
-        <div className="min-h-screen">
+        <div className="min-h-screen flex flex-col">
           <h2 className="text-7xl font-roboto font-bold">About</h2>
+          <section className="max-w-3xl mt-10">
+            <div className="max-w-xl text-gray-400 flex flex-col space-y-2">
+              <p>
+                We are a Latin American team that helps DAOs create, design and
+                develop Web3 primitives.
+              </p>
+              <p>
+                Radish solves client{"'"}s challenges by providing value,
+                ownership and trust. We are an engineering team that joins DAOs
+                face descentralization biases when creating software for
+                on-chain systems.
+              </p>
+              <p>
+                Wanna join us?{" "}
+                <Link href="#">
+                  <a className="text-white hover:underline">
+                    See our open positions.
+                  </a>
+                </Link>
+              </p>
+            </div>
+
+            <div className="flex flex-wrap gap-8 mt-12">
+              <Profile
+                image={asset_denny}
+                github="https://github.com/d3portillo"
+                twitter="https://twitter.com/d3portillo"
+                linkedin="https://linkedin.com/in/d3portillo"
+                name="Denny Portillo"
+              >
+                Frontend Engineer
+              </Profile>
+              <Profile
+                image={asset_valentin}
+                name="Valentin Vila"
+                linkedin="https://linkedin.com/in/valentin-vila"
+                github="https://github.com/DobleV55"
+              >
+                Backend Engineer
+              </Profile>
+              <Profile
+                image={asset_diego}
+                name="Diego Rubio"
+                github="https://github.com/zarpos"
+                linkedin="https://linkedin.com/in/diego-rubio-morato"
+              >
+                Solidity Engineer
+              </Profile>
+            </div>
+          </section>
         </div>
       </Layout>
     </Fragment>
+  )
+}
+
+function Profile({ name, image, children, github, twitter, linkedin }) {
+  return (
+    <div className="flex space-x-2">
+      <div className="w-20 h-20 rounded-full overflow-hidden">
+        <Image
+          width={420}
+          height={420}
+          src={image}
+          layout="responsive"
+          alt=""
+        />
+      </div>
+      <div className="flex flex-col">
+        <p className="text-gray-400 text-sm">{children}</p>
+        <h3 className="text-2xl font-montse">{name}</h3>
+        <div className="flex space-x-1">
+          {github && <ActionLink href={github} Icon={BsGithub} />}
+          {twitter && <ActionLink href={twitter} Icon={BsTwitter} />}
+          {linkedin && <ActionLink href={linkedin} Icon={FaLinkedinIn} />}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function ActionLink({ Icon, href }) {
+  return (
+    <Link href={href}>
+      <a
+        target="_blank"
+        className="p-2 hover:bg-white group rounded-full transition-colors duration-300"
+      >
+        <Icon className="text-lg group-hover:text-black transition-colors duration-300" />
+      </a>
+    </Link>
   )
 }
