2a46c5ade026a30da2b282ed25968b0f6ce4a27a
D3Portillo <d3portillo@gmail.com>
2022-10-24 17:22:32 +0200

    enh(Footer.js): add more tapping space for link elements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,18 +5,18 @@ import { BsGithub } from "react-icons/bs"
 
 function Footer() {
   return (
-    <footer className="flex mt-20 items-center border-t py-4 space-x-2">
+    <footer className="flex mt-20 items-center border-t py-4">
       <Link href="/">
         <a className="font-montse font-bold">Radish 2022</a>
       </Link>
       <div className="flex-grow" />
       <Link href="https://github.com/rabani-to">
-        <a target="_blank">
+        <a className="p-2" target="_blank">
           <BsGithub className="text-2xl" />
         </a>
       </Link>
       <Link href="https://twitter.com/d_rabanito">
-        <a target="_blank">
+        <a className="p-2" target="_blank">
           <IoLogoTwitter className="text-2xl" />
         </a>
       </Link>
