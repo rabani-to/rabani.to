7d61733eb8f515e096f7e0c665890d212bf6457a
D3Portillo <d3portillo@gmail.com>
2022-11-04 15:37:00 +0100

    enh(public,SeoTags.js): replace favicon

--- src/components/SeoTags.js
+++ src/components/SeoTags.js
@@ -3,12 +3,12 @@ import Head from "next/head"
 function SeoTags() {
   return (
     <Head>
-      <title>rabani.to</title>
+      <title>The Radish Team</title>
       <meta
         name="description"
         content="Your friendly people for on-chain systems."
       />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
       <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
